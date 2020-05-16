5f36dd8d9f5f7503f1f40422abac1772b937a074
DeathOcty <36937892+DeathOcty@users.noreply.github.com>
2020-05-16 17:29:46 -0400

    Hover Over Function

---
+++ GodotTyp/GameVariables.gd
@@ -0,0 +1,20 @@
+extends Node
+
+
+var CardHighlight = false;
+var CardNode = preload("res://Card.tscn")
+
+func _ready():
+	var newCard = CardNode.instance();
+	self.add_child(newCard)
+
+
+func CheckHigh():
+	if(!CardHighlight):
+		CardHighlight = true;
+		return true;
+	return false;
+
+
+func _process(_delta):
+	CardHighlight = false;
--- GodotTyp/mouseClick.gd
+++ GodotTyp/mouseClick.gd
@@ -1,22 +1,25 @@
 extends Area2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 var mouse_over = false
+var controlNode = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+
+# warning-ignore:return_value_discarded
 	connect("mouse_entered", self, "_mouse_over", [true])
+
+# warning-ignore:return_value_discarded
 	connect("mouse_exited",  self, "_mouse_over", [false])
+	controlNode = get_parent();
 	
 func _mouse_over(over):
 	self.mouse_over = over
 
 func _unhandled_input(event):
 	if mouse_over:
-		print(self.name + "MMOUSE");
+		if(controlNode.CheckHigh()):
+			print("NICE" + self.name); #Trigger Animation
 	if mouse_over and event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.is_pressed():
 		get_tree().set_input_as_handled()
 		clicked()
