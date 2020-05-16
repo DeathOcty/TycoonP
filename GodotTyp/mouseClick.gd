87aa85b609ae9c3e0edb71f93a37bc1223f42b77
DeathOcty <36937892+DeathOcty@users.noreply.github.com>
2020-05-16 03:03:13 -0400

    First Com

---
+++ GodotTyp/mouseClick.gd
@@ -0,0 +1,29 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var mouse_over = false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	connect("mouse_entered", self, "_mouse_over", [true])
+	connect("mouse_exited",  self, "_mouse_over", [false])
+	
+func _mouse_over(over):
+	self.mouse_over = over
+
+func _unhandled_input(event):
+	if mouse_over:
+		print("wooow");
+	if mouse_over and event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.is_pressed():
+		get_tree().set_input_as_handled()
+		clicked()
+
+func clicked():
+	print("sss");
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
